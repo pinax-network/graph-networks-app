23d8a5c3b8d00c619fde4669097e60ac41d00e0b
YaroShkvorets <shkvorets@gmail.com>
2024-11-14 16:56:34 -0500

    add view json button

--- src/components/NetworkDrawer.tsx
+++ src/components/NetworkDrawer.tsx
@@ -185,6 +185,8 @@ function ServiceSection({ type, services = [] }: ServiceSectionProps) {
 }
 
 export function NetworkDrawer({ network, subgraphCounts, onClose, isOpen }: NetworkDrawerProps) {
+  const [showJson, setShowJson] = useState(false);
+
   useEffect(() => {
     const handleEscapeKey = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
@@ -363,15 +365,35 @@ export function NetworkDrawer({ network, subgraphCounts, onClose, isOpen }: Netw
                 </div>
               </div>
             </div>
-            <div className="mt-8 pt-4 border-t border-gray-700 flex justify-end">
-              <InfoLink href="https://github.com/graphprotocol/networks-registry">
-                <div className="flex items-center gap-2">
-                  <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-                    <path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0012 2z" clipRule="evenodd" />
-                  </svg>
-                  Edit on GitHub
+            <div className="mt-8 pt-4 border-t border-gray-700">
+              <div className="flex justify-between items-center">
+                <button
+                  onClick={() => setShowJson(!showJson)}
+                  className="text-blue-400 hover:text-blue-300 transition-colors"
+                >
+                  {showJson ? 'Hide JSON' : 'View JSON'}
+                </button>
+
+                <InfoLink href="https://github.com/graphprotocol/networks-registry">
+                  <div className="flex items-center gap-2">
+                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+                      <path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0012 2z" clipRule="evenodd" />
+                    </svg>
+                    Edit on GitHub
+                  </div>
+                </InfoLink>
+              </div>
+
+              {showJson && (
+                <div className="mt-4 bg-slate-900 p-4 rounded-lg overflow-auto relative">
+                  <div className="absolute top-3 right-3">
+                    <CopyButton text={JSON.stringify(network, null, 2)} />
+                  </div>
+                  <pre className="text-white text-sm">
+                    {JSON.stringify(network, null, 2)}
+                  </pre>
                 </div>
-              </InfoLink>
+              )}
             </div>
           </div>
         </div>
