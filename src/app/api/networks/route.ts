c5a0f7783b540956e84967744ea79134fe7b91db
YaroShkvorets <shkvorets@gmail.com>
2024-12-20 11:19:34 -0500

    update registry domain

--- src/app/api/networks/route.ts
+++ src/app/api/networks/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const response = await fetch(
-      "https://registry.thegraph.com/TheGraphNetworksRegistry_v0_x_x.json",
+      "https://networks-registry.thegraph.com/TheGraphNetworksRegistry_v0_x_x.json",
       {
         next: { revalidate: 5 * 60 }, // Cache for 5 minutes
       }
